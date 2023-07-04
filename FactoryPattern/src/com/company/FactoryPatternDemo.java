763332dc5c1f81225da584e1a303af58bf972382
Amir Zarchini <amzarchini@gmail.com>
2023-07-04 07:51:54 +0330

    Update FactoryPatternDemo.java
    add comment

--- FactoryPattern/src/com/company/FactoryPatternDemo.java
+++ FactoryPattern/src/com/company/FactoryPatternDemo.java
@@ -7,10 +7,15 @@ public class FactoryPatternDemo {
     public static void main(String[] args) {
 	    ShapeFactory shapeFactory = new ShapeFactory();
 
+	// new instance from circle object
         Shape shape_circle = shapeFactory.getShape("circle");
         shape_circle.draw();
+
+	// new instance from rectangle object  
         Shape shape_rectangle = shapeFactory.getShape("rectangle");
         shape_rectangle.draw();
+
+	// new instance from square object
         Shape shape_square = shapeFactory.getShape("square");
         shape_square.draw();
 
